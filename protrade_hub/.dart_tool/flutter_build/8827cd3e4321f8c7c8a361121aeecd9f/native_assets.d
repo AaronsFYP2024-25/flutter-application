554e17db784da8a85ec0337f0038737bf0937a5a
AaronCronnelly <G00403528@gmit.ie>
2024-11-25 20:03:20 +0000

    still working on fixing emulator issue, trying to reslove all

--- protrade_hub/.dart_tool/flutter_build/8827cd3e4321f8c7c8a361121aeecd9f/native_assets.d
+++
@@ -1,1 +0,0 @@
- /Users/aaroncronnelly/Documents/Documents\ -\ MacBook/Uni/UNI\ YR4/projects/FYP/FinalYearProject/flutter-application/protrade_hub/.dart_tool/flutter_build/8827cd3e4321f8c7c8a361121aeecd9f/native_assets.yaml: 
